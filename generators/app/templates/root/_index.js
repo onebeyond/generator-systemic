e14be778a496643086c409037229cd2c0850ec27
feliun <felipe.polo@gmail.com>
2016-12-09 17:18:50 +0000

    Updating startup file

--- generators/app/templates/root/_index.js
+++ generators/app/templates/root/_index.js
@@ -1,31 +1,17 @@
-console.log('Hello world')
-// const system = require('./lib/system')
-// const runner = require('systemic-domain-runner')
-// const transports = require('./lib/system/transports')
-// const pkg = require('./package')
+const system = require('./lib/system')
+const runner = require('systemic-domain-runner')
+const bunyan = require('bunyan')
+const name = require('./package.json').name
+const log = bunyan.createLogger({ name: name })
 
-// process.env.SERVICE_ENV = process.env.SERVICE_ENV || 'local'
+process.env.SERVICE_ENV = process.env.SERVICE_ENV || 'local'
 
-// runner(system).start((err, components) => {
-//     if (err) die('Error starting system', err)
-//     process.on('confabulous_reload_error', (err) => components.logger.error('Error reloading config', err))
-// })
+runner(system).start((err, components) => {
+    if (err) die('Error starting system', err)
+    process.on('confabulous_reload_error', (err) => components.logger.error('Error reloading config', err))
+})
 
-// function die(message, err) {
-//     const event = {
-//         timestamp: new Date().toISOString(),
-//         level: 'error',
-//         message: message,
-//         service: {
-//             name: pkg.name,
-//             env: process.env['SERVICE_ENV']
-//         },
-//         error: {
-//             message: err.message,
-//             stack: err.stack,
-//             code: err.code
-//         }
-//     }
-//     transports[process.env.LOGGER_TRANSPORT || 'console'](event)
-//     process.exit(1)
-// }
+function die(message, err) {
+    log.error(err, message)
+    process.exit(1)
+}
