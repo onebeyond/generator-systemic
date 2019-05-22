4bd1b96f415d5f8509648aff3ec086cb0087c271
Jose Antonio <josegrobles1997@gmail.com>
2019-05-22 15:47:09 +0200

    Improve logging system

--- generators/app/templates/lib/components/logging/console.js
+++ generators/app/templates/lib/components/logging/console.js
@@ -6,7 +6,7 @@ const response = hogan.compile(
 	'{{{timestamp}}} | {{{displayLevel}}} [{{package.name}}] {{{displayTracer}}} {{{request.method}}} {{{response.statusCode}}} {{{request.url}}}',
 );
 const error = hogan.compile(
-	'{{{timestamp}}} | {{{displayLevel}}} [{{package.name}}] {{{displayTracer}}} {{{message}}} {{{code}}}\n{{{error.stack}}} {{{details}}}',
+	'{{{timestamp}}} | {{{displayLevel}}} [{{package.name}}] {{{displayTracer}}} {{{errorMessage}}} {{{code}}}\n{{{error.stack}}} {{{details}}}',
 );
 const info = hogan.compile(
 	'{{{timestamp}}} | {{{displayLevel}}} [{{package.name}}] {{{displayTracer}}} {{{message}}} {{{details}}}',
@@ -27,12 +27,15 @@ module.exports = () => {
 			displayTracer: R.has('tracer', event) ? event.tracer.substr(0, 6) : '|',
 			displayLevel: event.level.toUpperCase(),
 			details: Object.keys(details).length ? `\n ${JSON.stringify(details, null, 2)}` : '',
+			errorMessage:
+				event.error &&
+				(event.error.message instanceof Object ? JSON.stringify(event.error.message) : event.error.message),
 			timestamp: event.timestamp.toISOString(),
 		});
 		const colour = colours[event.level] || colours.default;
 		const log = console[event.level] || console.info; // eslint-disable-line no-console
 		if (R.has('response.statusCode', event)) log(colour(response.render(data)));
-		else if (R.has('error.message', event)) log(colour(error.render(data)));
+		else if (data.errorMessage) log(colour(error.render(data)));
 		else log(colour(info.render(data)));
 	};
 
