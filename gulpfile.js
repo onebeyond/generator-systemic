e04e29910504c11798b0067e14d126b67086dcbe
Josep Servat <jservatlorca@gmail.com>
2019-05-26 21:45:32 +0200

    Remove gulp-nsp
    nsp will shutdown, we should use npm audit to check vulnerabilitie in
    node packages.
    
    https://blog.npmjs.org/post/175511531085/the-node-security-platform-service-is-shutting
    guidesmiths/generator-systemic#4

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
 gulp.task('static', function () {
@@ -14,10 +13,6 @@ gulp.task('static', function () {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
-
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
 });
 
 gulp.task('pre-test', function () {
@@ -48,5 +43,5 @@ gulp.task('watch', function () {
   gulp.watch(['generators/**/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('prepublish', ['nsp']);
+gulp.task('prepublish', []);
 gulp.task('default', ['static', 'test']);
