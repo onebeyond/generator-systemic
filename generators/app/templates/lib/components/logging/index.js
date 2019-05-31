774d3aea39c786f0df3ebc9171627337b7a88254
Josep Servat <jservatlorca@gmail.com>
2019-05-31 13:19:37 +0200

    Bump systemic version (#8)

--- generators/app/templates/lib/components/logging/index.js
+++ generators/app/templates/lib/components/logging/index.js
@@ -1,18 +1,23 @@
 const System = require('systemic');
 const prepper = require('./prepper');
-const console = require('./console');
 const bunyan = require('./bunyan');
+const console = require('./console');
+
 const prepperMiddleware = require('./prepper-middleware');
 
 module.exports = new System({ name: 'logging' })
-  .add('transports.console', console())
-  .add('transports.bunyan', bunyan()).dependsOn('pkg')
-  .add('transports')
-  .dependsOn(
-    { component: 'transports.console', destination: 'console' },
-    { component: 'transports.bunyan', destination: 'bunyan' }
-  )
-  .add('logger', prepper())
-  .dependsOn('config', 'pkg', 'transports')
-  .add('middleware.prepper', prepperMiddleware())
-  .dependsOn('app');
+	.add('transports.console', console())
+	.add('transports.bunyan', bunyan())
+	.dependsOn('pkg')
+	.add('transports')
+	.dependsOn({
+		component: 'transports.console',
+		destination: 'console'
+	}, {
+		component: 'transports.bunyan',
+		destination: 'bunyan'
+	} )
+	.add('logger', prepper())
+	.dependsOn('config', 'pkg', 'transports')
+	.add('middleware.prepper', prepperMiddleware())
+	.dependsOn('app');
