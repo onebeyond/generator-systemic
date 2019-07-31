a4f351a779f3053285c9c5c932450de64ce94069
feliun <felipe.polo@gmail.com>
2019-07-31 16:26:44 +0200

    Fixing config generation

--- generators/app/templates/lib/components/config/confabulous.js
+++ generators/app/templates/lib/components/config/confabulous.js
@@ -5,24 +5,22 @@ module.exports = ({ confabulous } = {}) => {
 	const Confabulous = confabulous || Conf;
 	const { loaders } = Confabulous;
 
-	const start = async () =>
-		new Promise((resolve, reject) => {
-			new Confabulous()
-				.add(() => loaders.require({ path: path.join(process.cwd(), 'config', 'default.js'), watch: true }))
-				.add(() =>
-					loaders.require({
-						path: path.join(process.cwd(), 'config', `${process.env.SERVICE_ENV}.js`),
-						mandatory: false,
-					})
-				)
-				.add(() =>
-					loaders.require({ path: path.join(process.cwd(), 'secrets', 'secrets.json'), watch: true, mandatory: false })
-				)
-				.add(() => loaders.args())
-				.on('loaded', resolve)
-				.on('error', reject)
-				.end(resolve);
-		});
+	const start = (params, cb) =>
+		new Confabulous()
+			.add(() => loaders.require({ path: path.join(process.cwd(), 'config', 'default.js'), watch: true }))
+			.add(() =>
+				loaders.require({
+					path: path.join(process.cwd(), 'config', `${process.env.SERVICE_ENV}.js`),
+					mandatory: false,
+				}),
+			)
+			.add(() =>
+				loaders.require({ path: path.join(process.cwd(), 'secrets', 'secrets.json'), watch: true, mandatory: false }),
+			)
+			.add(() => loaders.args())
+			.on('loaded', cb)
+			.on('error', cb)
+			.end(cb);
 
 	return { start };
 };
