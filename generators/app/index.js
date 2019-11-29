6b4c4592e8d4bd5ac2d16b57d67c6353eafe3965
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-11-29 15:41:51 +0100

    Migrated generator to latest version
    - Related #23

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,14 +1,20 @@
-const yeoman = require('yeoman-generator');
-const _ = require('lodash');
+
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
 const yosay = require('yosay');
 const fs = require('fs');
 const path = require('path');
 
+
 const templatesFolder = path.join(__dirname, 'templates');
 
-module.exports = yeoman.Base.extend({
+module.exports = class extends Generator {
 	prompting() {
-		return this.prompt([{
+		this.log(
+			yosay(`Welcome to the unreal ${chalk.red('systemic')} generator!`),
+		);
+
+		const prompts = [{
 			type: 'input',
 			name: 'name',
 			message: 'Your service name',
@@ -19,40 +25,41 @@ module.exports = yeoman.Base.extend({
 			name: 'description',
 			message: 'A brief description',
 			default: '',
-		}]).then(_answers => {
-			this.props = _answers;
+		}];
+
+		return this.prompt(prompts).then(props => {
+			this.props = props;
 			this.props.components = ['app', 'config', 'logging', 'express', 'routes'];
 		});
-	},
-	writing: {
-		config() {
-			this._copyFiles('config', 'config');
-			this._copyFiles('root', '.');
-		},
-		app() {
-			this._copyAppFiles();
-		},
-	},
+	}
+
+	writing() {
+		const copyFiles = (from, to) => {
+			const configFiles = fs.readdirSync(path.join(templatesFolder, from));
+			configFiles.forEach(file => {
+				this.fs.copyTpl(this.templatePath(`./${from}/${file}`), this.destinationPath(`${to}/${file.replace(/^_/, '')}`), this.props);
+			});
+		};
+
+		const copyAppFiles = () => {
+			this.fs.copy(this.templatePath('./test/.eslintrc'), this.destinationPath('./test/.eslintrc'));
+			this.fs.copy(this.templatePath('./test/*'), this.destinationPath('./test/'));
+			this.props.components.forEach(component => {
+				this.fs.copy(this.templatePath(`./lib/components/${component}/*`), this.destinationPath(`./components/${component}/`));
+			});
+		};
+
+		copyFiles('config', 'config');
+		copyFiles('root', '.');
+		copyAppFiles();
+	}
+
 	install() {
-		return this.installDependencies({ npm: true, bower: false });
-	},
+		this.installDependencies({ npm: true, bower: false });
+	}
+
 	end() {
 		const outputMsg = `\n\nYour service ${this.props.name} has been created.\nnpm run start - start your systemic service`;
 		this.log(yosay(outputMsg));
-	},
-	_copyFiles(from, to) {
-		const configFiles = fs.readdirSync(path.join(templatesFolder, from));
-		const self = this;
-		_.forEach(configFiles, file => {
-			self.fs.copyTpl(self.templatePath(`./${from}/${file}`), self.destinationPath(`${to}/${file.replace(/^_/, '')}`), self.props);
-		});
-	},
-	_copyAppFiles() {
-		const self = this;
-		this.fs.copy(this.templatePath('./test/.eslintrc'), this.destinationPath('./test/.eslintrc'));
-		this.fs.copy(this.templatePath('./test/*'), this.destinationPath('./test/'));
-		_.forEach(this.props.components, component => {
-			self.fs.copy(self.templatePath(`./lib/components/${component}/*`), self.destinationPath(`./components/${component}/`));
-		});
-	},
-});
+	}
+};
