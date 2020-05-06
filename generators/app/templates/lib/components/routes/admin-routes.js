14660494b51bc1b9ab6da55329f3f5d5bb860e74
MatteoDiPaolo <matteo.dipaolantonio@guidesmiths.com>
2020-05-06 18:16:47 +0200

    feat: replaces express-swagger-generator with swagger-endpoint-validator

--- generators/app/templates/lib/components/routes/admin-routes.js
+++ generators/app/templates/lib/components/routes/admin-routes.js
@@ -1,24 +1,13 @@
-const expressSwaggerGenerator = require('express-swagger-generator');
+const bodyParser = require('body-parser');
+const validator = require('swagger-endpoint-validator');
 
 module.exports = () => {
 	const start = async ({ manifest = {}, app, config }) => {
-		const { swaggerOptions } = config;
-		const expressSwagger = expressSwaggerGenerator(app);
-		const options = {
-			swaggerDefinition: {
-				...swaggerOptions.swaggerDefinition,
-			},
-			basedir: __dirname,
-			files: ['./**/**-routes.js'],
-		};
-		expressSwagger(options);
+		app.use(bodyParser.urlencoded({ extended: true }));
+		app.use(bodyParser.json());
 
-		/**
-		 * This endpoint serves the manifest
-		 * @route GET /__/manifest
-		 * @group Admin - Everything about admin routes
-		 * @returns 200 - Sucessful response
-		*/
+		await validator.init(app, config.swaggerValidator);
+
 		app.get('/__/manifest', (req, res) => res.json(manifest));
 
 		return Promise.resolve();
