c272f04161e7d9842b61f275e8e0599bcf96f91f
kevin Martínez <kevinccbsg@gmail.com>
2020-05-07 01:30:31 +0200

    feat: add error-handler-module to the template

--- generators/app/templates/lib/components/routes/admin-routes.js
+++ generators/app/templates/lib/components/routes/admin-routes.js
@@ -1,4 +1,7 @@
 const expressSwaggerGenerator = require('express-swagger-generator');
+const {
+	handleHttpError, tagError, errorFactory, CustomErrorTypes,
+} = require('error-handler-module');
 
 module.exports = () => {
 	const start = async ({ manifest = {}, app, config }) => {
@@ -20,6 +23,23 @@ module.exports = () => {
 		 * @returns 200 - Sucessful response
 		*/
 		app.get('/__/manifest', (req, res) => res.json(manifest));
+
+		/**
+		 * This is an example endpoint of error handling module usage you can remove it
+		 * @route GET /__/manifest
+		 * @group Admin - Everything about admin routes
+		 * @returns 200 - Sucessful response
+		*/
+		app.get('/__/error', async (req, res, next) => {
+			try {
+				const badRequestError = errorFactory(CustomErrorTypes.BAD_REQUEST);
+				throw badRequestError('Bad request error example', 'extra info');
+			} catch (err) {
+				return next(tagError(err));
+			}
+		});
+
+		app.use(handleHttpError(logger));
 
 		return Promise.resolve();
 	};
